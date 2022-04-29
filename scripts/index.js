3adcd6ea8d676317a71c7b9539a4c937f3c172c5
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-04-29 20:23:45 +0300

    Исправлены блоки profile, popup, исправлена семантика и классы по БЭМу, изменены соответствующие стили и логика JS по замечаниям

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,45 @@
-const editProfileBtn = document.querySelector('.profile__info_edit-button');
+// Объявление переменных
 const popup = document.querySelector('.popup');
+const editProfileBtn = document.querySelector('.profile__edit-button');
 const popupCloseBtn = document.querySelector('.popup__close-button');
-
-editProfileBtn.addEventListener('click', function () {
-  togglePopup();
-});
-
-popupCloseBtn.addEventListener('click', function () {
-  togglePopup();
-});
-
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
-};
-
+// Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form_input-name');
-let jobInput = document.querySelector('.popup__form_input-job');
-let profileName = document.querySelector('.profile__info_name');
-let profileJob = document.querySelector('.profile__info_comment');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__form_input_name');
+let jobInput = document.querySelector('.popup__form_input_job');
+// Находим поля профиля в DOM
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
+//Обработчик открытия формы
+function openPopup() {
+  if (popup.classList.contains('popup_opened')) {
+
+  } else {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
+};
+//Обработчик закрытия формы
+function closePopup() {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+  }
+};
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  nameInput.getAttribute('value');
-  jobInput.getAttribute('value');
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglePopup();
+  closePopup();
 };
 
+//Обработчик события открытия формы
+editProfileBtn.addEventListener('click', openPopup);
+//Обработчик события закрытия формы
+popupCloseBtn.addEventListener('click', closePopup);
+//Обработчик события "отпрвки" формы
 formElement.addEventListener('submit', formSubmitHandler);
 
