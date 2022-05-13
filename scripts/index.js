720ba5cc1be46de90c91a1a15f909211f5b32ddd
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-05-13 22:07:25 +0300

    fix: исправлена логика отрисовки и добавления карточек, ткрытия попапов, массив выведен в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,106 +1,50 @@
-// Объявление переменных
-const initialElements = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-//объявляем переменные для попапа редактировния профиля
-const profileEditBtn = document.querySelector('.profile__edit-button');
-const popupTypeEdit = document.querySelector('.popup_type_edit');
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__form_input_name');
-let jobInput = document.querySelector('.popup__form_input_job');
-// Находим поля профиля в DOM
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-//объявляем переменную для закрытия попапов
-const popupCloseBtn = document.querySelectorAll('.popup__close-button');
-//переменная-шаблон карточки
-const elementTemplate = document.querySelector('.template-element').content;
 //Переменная-контейнер с карточками
 const elements = document.querySelector('.elements');
-
+//переменная-шаблон карточки
+const elementTemplate = document.querySelector('.template-element').content;
+//объявляем переменную для закрытия попапов
+const popupCloseBtn = document.querySelectorAll('.popup__close-button');
+//объявляем переменные для попапа редактировния профиля
+const profileEditBtn = document.querySelector('.profile__edit-button');
+// Находим форму редактирования в DOM
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const formElementEdit = popupTypeEdit.querySelector('.popup__form_edit-profile');
+// Находим поля формы редактирования в DOM
+const nameInput = formElementEdit.querySelector('.popup__form_input_name');
+const jobInput = formElementEdit.querySelector('.popup__form_input_job');
+// Находим поля профиля в DOM
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 //Объявляем переменные для попапа добавления карточек
 const elementAddBtn = document.querySelector('.profile__add-button');
 const popupTypeAdd = document.querySelector('.popup_type_add');
-const elementTitleInput = document.querySelector('.popup__form_input_element-title');
-const elementLinkInput = document.querySelector('.popup__form_input_element-link');
-const popupFormTypeAdd = document.querySelector('.popup__form_add-element');
+const formElementAdd = popupTypeAdd.querySelector('.popup__form_add-element');
+// Находим поля формы добавления в DOM
+const elementTitleInput = formElementAdd.querySelector('.popup__form_input_element-title');
+const elementLinkInput = formElementAdd.querySelector('.popup__form_input_element-link');
 //Объявляем переменные попапа просмотра фотографий
 const popupTypeView = document.querySelector('.popup_type_view');
-const popupDescription = document.querySelector('.popup__description');
-const popupImage = document.querySelector('.popup__image');
+const popupViewFigure = popupTypeView.querySelector('.popup__figure');
+const popupDescription = popupTypeView.querySelector('.popup__description');
+const popupImage = popupTypeView.querySelector('.popup__image');
 
-
-
-//Обработчик открытия формы редактирования профиля
-function openPopup() {
-    popupTypeEdit.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+//Функция открытия форм
+function openPopup(popupType) {
+  popupType.classList.add('popup_opened');
 };
-
-//Обработчик закрытия форм
-const closePopup = (evt) => {
-  evt.target.closest('.popup').classList.remove('popup_opened');
+//Функция закрытия форм
+function closePopup(popupType) {
+  popupType.classList.remove('popup_opened');
 };
-// Обработчик «отправки» формы, хотя пока
+// Обработчик «отправки» формы редактирования профиля, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup(evt);
+  closePopup(popupTypeEdit);
 };
-//Функция открытия формы добавления карточки
-const handleAddElement = (evt) => {
- popupTypeAdd.classList.add('popup_opened');
- elementTitleInput.value = "";
- elementLinkInput.value = "";
-};
-//Обработчик "отправки" формы на добавление карточки
-const formSubmitHandlerAdd = (evt) => {
-  evt.preventDefault();
-  const element = elementTemplate.cloneNode(true);
-  const elementPlaceName = element.querySelector('.element__place-name');
-  const elementImage = element.querySelector('.element__image');
-  const elementDeleteBtn = element.querySelector('.element__delete-button');
-  const elementLikeBtn = element.querySelector('.element__like-button');
 
-  elementLikeBtn.addEventListener('click', handleLikeClick);
-  elementDeleteBtn.addEventListener('click', handleDeleteClick);
-  elementImage.addEventListener('click', handleImageClick);
-
-  elementPlaceName.textContent = elementTitleInput.value;
-  elementImage.src = elementLinkInput.value;
-  elementImage.alt = elementTitleInput.value;
-  elementPlaceName.textContent = elementTitleInput.value;
-  elements.prepend(element);
-  closePopup(evt);
-};
 //Функция добавления лайка карточке
 const handleLikeClick = (evt) => {
   evt.target.classList.toggle('element__like-button_active');
@@ -109,38 +53,60 @@ const handleLikeClick = (evt) => {
 const handleDeleteClick = (evt) => {
   evt.target.closest('.element').remove();
 };
-//Функция открытия попапа с картинкой
-const handleImageClick = (evt) => {
-  popupTypeView.classList.add('popup_opened');
-  popupDescription.textContent = evt.target.alt;
-  popupImage.src = evt.target.src;
-};
 //Обработчик события открытия формы редактироваиня профиля
-profileEditBtn.addEventListener('click', openPopup);
+profileEditBtn.addEventListener('click', function() {
+  openPopup(popupTypeEdit);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+//Обработчик события открытия формы добавления карточки
+elementAddBtn.addEventListener('click', function() {
+  openPopup(popupTypeAdd);
+  elementTitleInput.value = "";
+  elementLinkInput.value = "";
+});
 //Обработчик события закрытия форм
-for (let i = 0; i < popupCloseBtn.length; ++i) {
-  popupCloseBtn[i].addEventListener('click', closePopup)
-};
+  for (let i = 0; i < popupCloseBtn.length; ++i) {
+    popupCloseBtn[i].addEventListener('click', function() {
+      const popupCloseBtnArray = Array.from(popupCloseBtn)
+      if (popupCloseBtnArray[i].nextElementSibling === formElementEdit) {
+        closePopup(popupTypeEdit);
+      } else if (popupCloseBtnArray[i].nextElementSibling === formElementAdd) {
+        closePopup(popupTypeAdd);
+      } else if  (popupCloseBtn[i].parentElement === popupViewFigure) {
+        closePopup(popupTypeView);
+      } else {
+        return popupCloseBtnArray[i];
+      }
+    })
+  };
 //Обработчик события "отпрвки" формы
-formElement.addEventListener('submit', formSubmitHandler);
-//Добавление шести карточек из "коробки"
-initialElements.forEach(({name, link}) => {
-  const element = elementTemplate.cloneNode(true);
-  const elementImage = element.querySelector('.element__image');
+formElementEdit.addEventListener('submit', formSubmitHandler);
+//Функция создания новой карточки
+const createElement = ({name, link}) => {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
   const elementPlaceName = element.querySelector('.element__place-name');
+  const elementImage = element.querySelector('.element__image');
   const elementDeleteBtn = element.querySelector('.element__delete-button');
   const elementLikeBtn = element.querySelector('.element__like-button');
-
-  elementAddBtn.addEventListener('click', handleAddElement);
-  elementLikeBtn.addEventListener('click', handleLikeClick);
-  elementDeleteBtn.addEventListener('click', handleDeleteClick);
-  elementImage.addEventListener('click', handleImageClick);
   elementImage.src = link;
   elementImage.alt = name;
   elementPlaceName.textContent = name;
-  elements.append(element);
-  //Обработчик события добавления карточки
-  popupFormTypeAdd.addEventListener('submit', formSubmitHandlerAdd);
+  elementLikeBtn.addEventListener('click', handleLikeClick);
+  elementDeleteBtn.addEventListener('click', handleDeleteClick);
+  elementImage.addEventListener('click', (evt) => {
+    openPopup(popupTypeView);
+    popupDescription.textContent = evt.target.alt;
+    popupImage.src = evt.target.src;
+  });
+  return element;
+};
+//Добавление шести карточек из "коробки"
+initialElements.forEach( (item) => elements.append(createElement(item)));
+//Обработчик события добавления карточки
+formElementAdd.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const newElement = createElement({name: elementTitleInput.value, link: elementLinkInput.value});
+  elements.prepend(newElement);
+  closePopup(popupTypeAdd);
 });
-
-
